5654a34be523eac8e60457fb2a2a1ce675c8f571
sumitpandat1707 <st119420@gmail.com>
2025-04-24 12:29:11 +0530

    Initial commit

--- task_management/settings.py
+++ task_management/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-hv$90p-ra7=4xvzq+gs^2_lg=s-c3a4jj(z2ps8kem4!a(2s7)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sumit-6xk5.onrender.com']
+ALLOWED_HOSTS = ['sumit-6xk5.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 
